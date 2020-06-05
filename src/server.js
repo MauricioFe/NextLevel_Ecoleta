d9e22584f278b16c820c07efdca011b218e60dfc
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-05 15:30:32 -0300

    Criação do insert no banco

--- src/server.js
+++ src/server.js
@@ -8,6 +8,9 @@ const db = require("./database/db")
 server.use(express.static("public"))
 
 
+//habilitar o reqbody
+server.use(express.urlencoded({ extended: true }))
+
 /*utilizando tamplate engine*/
 const nunjucks = require("nunjucks")
 nunjucks.configure("src/views", {
@@ -23,9 +26,17 @@ nunjucks.configure("src/views", {
 server.get("/", (req, res) => {
     return res.render("index.html")
 })
+
+
+
 server.get("/create-point", (req, res) => {
     return res.render("create-point.html")
 })
+
+
+
+
+
 server.get("/search", (req, res) => {
 
     db.all(`Select * from places`, function (err, rows) {
@@ -36,10 +47,38 @@ server.get("/search", (req, res) => {
         console.log(rows)
         const total = rows.length;
         //mostrar a página html com os dadoss
-    return res.render("search-results.html",{places: rows, count: total})
+        return res.render("search-results.html", { places: rows, count: total })
     })
 
 })
 
+
+server.post("/savepoint", (req, res) => {
+    //req.body
+    const query = `INSERT INTO places (nome, foto, address, address2, state, city, items) 
+    values (?,?,?,?,?,?,?);`
+    const values = [
+        req.body.nome,
+        req.body.foto,
+        req.body.address,
+        req.body.address2,
+        req.body.state,
+        req.body.city,
+        req.body.items
+    ]
+
+    function afterInsertData(err) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Cadastrado com sucesso")
+        console.log(this)
+
+        return res.render("/create-point", { saved: true })
+    }
+
+    db.run(query, values, afterInsertData)
+
+})
 //liga o servidor
 server.listen(3000)
