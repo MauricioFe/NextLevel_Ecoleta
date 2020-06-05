3e17e06b9c027a9d190af8afbdd2f7bf2122501d
MauricioFe <mauricio.lacerdaml@gmail.com>
2020-06-05 16:39:09 -0300

    Mensagem de cadastro

--- src/server.js
+++ src/server.js
@@ -34,25 +34,6 @@ server.get("/create-point", (req, res) => {
 })
 
 
-
-
-
-server.get("/search", (req, res) => {
-
-    db.all(`Select * from places`, function (err, rows) {
-        if (err) {
-            return console.log(err)
-        }
-        console.log("Aqui estão seus registros: ")
-        console.log(rows)
-        const total = rows.length;
-        //mostrar a página html com os dadoss
-        return res.render("search-results.html", { places: rows, count: total })
-    })
-
-})
-
-
 server.post("/savepoint", (req, res) => {
     //req.body
     const query = `INSERT INTO places (nome, foto, address, address2, state, city, items) 
@@ -69,16 +50,41 @@ server.post("/savepoint", (req, res) => {
 
     function afterInsertData(err) {
         if (err) {
-            return console.log(err)
+            console.log(err)
+            return res.send("Erro ao realizar cadastro")
         }
         console.log("Cadastrado com sucesso")
         console.log(this)
 
-        return res.render("/create-point", { saved: true })
+        return res.render("create-point.html",{ saved: true })
     }
 
     db.run(query, values, afterInsertData)
 
 })
+
+
+
+server.get("/search", (req, res) => {
+
+    const search = req.query.search
+    if  (search == ""){
+        return res.render("search-results.html", { count: 0 })
+    }
+
+    db.all(`Select * from places where city like '%${search}%'`, function (err, rows) {
+        if (err) {
+            return console.log(err)
+        }
+        console.log("Aqui estão seus registros: ")
+        console.log(rows)
+        const total = rows.length;
+        //mostrar a página html com os dadoss
+        return res.render("search-results.html", { places: rows, count: total })
+    })
+
+})
+
+
 //liga o servidor
 server.listen(3000)
